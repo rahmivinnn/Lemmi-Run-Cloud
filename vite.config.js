f2e62f868adf61176d05cb94272faa9013bb1dc5
rahmivinnn <your-email@example.com>
2025-08-26 15:17:43 +0700

    Fix vite.config.js with path alias for @ imports

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   root: '.', // Set root to current directory
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
   build: {
     outDir: 'dist',
     rollupOptions: {
